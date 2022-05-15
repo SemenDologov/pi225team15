3aef2fbfebe6c6a239140357accb2ba7274faef1
Bikbulatov Danil <donil123123@gmail.com>
2022-05-15 16:11:53 +0500

    Bukbulatov: created abstract class calculation.java

---
+++ src/main/java/project4/calculation.java
@@ -0,0 +1,26 @@
+package project4;
+
+public abstract class calculation {
+
+	   private int length;
+	   private int width;
+
+	   public abstract void Calc();
+
+	   public int getLength() {
+	       return length;
+	   }
+
+	   public void setLength(int length) {
+	       this.length = length;
+	   }
+
+	   public int getWidth() {
+	       return width;
+	   }
+
+	   public void setWidth(int width) {
+	       this.width = width;
+	   }
+}
+
